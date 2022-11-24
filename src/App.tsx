6411f8ab6129ee6e957ee348f0d28a6cd3e4f057
Bruno Juwer <bruno.juwer1997@gmail.com>
2022-11-24 16:15:56 -0300

    Added useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
+
+import { CyclesContextProvider } from './context/CyclesContext'
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
-import { BrowserRouter } from 'react-router-dom'
-import { Router } from './Router'
-import { CyclesContextProvider } from './context/CyclesContext'
 
 export function App() {
   return (
